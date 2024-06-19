86c3912600911a3ed10ca7efdbf5737e7d7aa77f
Melvin <melsava1207@gmail.com>
2024-06-19 13:04:55 -0600

    audio a texto terminado

--- JAVASCRIPT/AUDIO-TEXTO/audiotexto.js
+++ JAVASCRIPT/AUDIO-TEXTO/audiotexto.js
@@ -1,1 +1,23 @@
-const 
+//se obtiene el boton para comenzar a hablar
+const btn = document.querySelector('#btn-aud')
+const texto = document.querySelector('#texto')
+
+
+btn.addEventListener('click', () => {
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+  
+  const recognition = new SpeechRecognition()
+  recognition.interimResults = true
+
+  recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+
+      console.log(transcript)
+
+      texto.innerHTML = transcript
+  })
+
+    recognition.start()
+})
